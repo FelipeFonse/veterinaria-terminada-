eea6a512aa5d16543178c37f9183c1dc0bc4d624
cesar Fonseca bermudez <cesarfb2001@gmail.com>
2026-02-27 01:18:06 -0500

    terminado

---
+++ src/Main.java
@@ -0,0 +1,85 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String noMbrem, espeCie,continuar;
+        int edAd;
+        double peSo;
+        int dat = 1;
+
+    do
+
+    {
+        System.out.println("----------REGISTRO DE MASCOTAS------------");
+        System.out.println(" Nombre ");
+        noMbrem = sc.nextLine();
+        System.out.println(" Especie ");
+        espeCie = sc.nextLine();
+        System.out.println(" Edad ");
+        edAd = sc.nextInt();
+        System.out.print("  Peso(kg) : ");
+        peSo = sc.nextDouble();
+        sc.nextLine();
+
+        System.out.print("¿Los datos son correctos? (si/no): ");
+        continuar = sc.nextLine().trim().toLowerCase(); /* esta cadena de caracteristicas se
+        usan para determinar errorres en string como mayuscolas espacios y lee los que oingrese el usuario
+*/
+    } while (continuar.equals("no"));
+        Mascotas2 miMascota = new Mascotas2(noMbrem, espeCie, edAd, peSo);
+
+
+
+        int opcion;
+     do
+
+    {
+
+        System.out.println(" CLÍNICA VETERINARIA 🐾      ");
+        System.out.println("1. Mostrar información       ");
+        System.out.println("2. Alimentar mascota         ");
+        System.out.println("3. Vacunar mascota           ");
+        System.out.println("4. Cumpleaños  (+ 1 año)     ");
+        System.out.println("0. Salir                     ");
+        System.out.print("Seleccione una opción: ");
+        opcion = sc.nextInt();
+
+        switch (opcion) {
+            case 1:
+                miMascota.mostrarDatos();
+                break;
+            case 2:
+                System.out.print("Cantidad de alimento (kg): ");
+                double cantidad = sc.nextDouble();
+                miMascota.alimentar(cantidad);
+                break;
+            case 3:
+                miMascota.vacunar();
+                break;
+            case 4:
+                miMascota.cumplirAnios();
+                break;
+            case 0:
+                System.out.println("👋 Saliendo...");
+                break;
+            default:
+                System.out.println("⚠️  Opción no válida.");
+        }
+
+    } while(opcion !=0);
+
+
+
+        System.out.println("   ESTADO FINAL DE LA MASCOTA");
+
+        miMascota.mostrarDatos();
+
+        sc.close();
+
+
+}
+}
---
+++ src/Mascotas2.java
@@ -0,0 +1,102 @@
+public class Mascotas2 {
+
+    private String nom;
+    private String especie;
+    private int edad;
+    private double peso;
+    private boolean vacuna;
+
+    public Mascotas2(String nom, String especie, int edad, double peso) {
+        this.nom = nom;
+        this.especie = especie;
+        this.edad = edad;
+        this.peso = peso;
+        this.vacuna = false;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public boolean isVacuna() {
+        return vacuna;
+    }
+
+    public void setVacuna(boolean vacuna) {
+        this.vacuna = vacuna;
+    }
+
+    public Mascotas2() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Mascotas2{" +
+                "nom='" + nom + '\'' +
+                ", especie='" + especie + '\'' +
+                ", edad=" + edad +
+                ", peso=" + peso +
+                ", vacuna=" + vacuna +
+                '}';
+    }
+    public void vacunar() {
+        this.vacuna = true;
+        System.out.println(" -" + nom + " ha sido vacunado/a.");
+    }
+
+
+    public void alimentar(double cantidad) {
+        if (cantidad < 0) {
+            System.out.println("⚠️  Error: no se permiten cantidades negativas.");
+        } else {
+            this.peso += cantidad;
+            System.out.println("🍖 " + nom + " fue alimentado/a. Nuevo peso: " + peso + " kg.");
+        }
+    }
+
+
+    public void cumplirAnios() {
+        this.edad++;
+        System.out.println("🎂 " + nom + " cumplió un año más Edad actual: " + edad);
+    }
+    public void mostrarDatos(){
+        System.out.println("─────────────────────────────────");
+        System.out.println("📋 INFORMACIÓN DE LA MASCOTA");
+        System.out.println("─────────────────────────────────");
+        System.out.println("  Nombre  : " + nom);
+        System.out.println("  Especie : " + especie);
+        System.out.println("  Edad    : " + edad + " año(s)");
+        System.out.println("  Peso    : " + peso + " kg");
+        System.out.println("  Vacunado: "+ vacuna +" ");
+
+    }
+}
